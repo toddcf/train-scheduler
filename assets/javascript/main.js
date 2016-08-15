0f69a01962d07e69ff731dbd981ebb6c51eb95aa
Todd Croak-Falen <Todd Croak-Falen>
2016-08-14 23:38:21 -0700

    Added table, form, and pseudo code.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,7 @@
 alert("JavaScript link working!");
+
+// Get current time (from Moment JS)
+
+// Calculate next train's arrival time using current time.
+
+// Users from many different machines must be able to view the same train times.
