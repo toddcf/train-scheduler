b4f7c56176bd1e13ef6a0797b883176608bc0100
Todd Croak-Falen <Todd Croak-Falen>
2016-08-16 23:02:25 -0700

    Added link to Moment JS.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,14 @@
 alert("JavaScript link working!");
 
+// In this assignment, you'll create a train schedule application that
+// incorporates Firebase to host arrival and departure data.
+// Your app will retrieve and manipulate this information with Moment.js.
+// This website will provide up-to-date information about various trains,
+// namely their arrival times and how many minutes remain until they
+// arrive at their station.
+
+
+
 // Get current time (from Moment JS)
 
 // Calculate next train's arrival time using current time.
